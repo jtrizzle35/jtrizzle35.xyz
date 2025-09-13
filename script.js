80eb0a417c9fc385fa036beb591d4552013e1331
jtrizzle35 <jtrizzle35@gmail.com>
2025-09-13 01:07:43 -0400

    Update script.js
    attempting format of text with dl dt

--- script.js
+++ script.js
@@ -67,11 +67,11 @@ async function fetchAndDisplayStuckSongs() {
             const songTitle = song.Song || 'Unknown Song';
 
             displayHtml += `
-                <h3>  
-                    <strong>${date}</strong>
-                    ${artist}:
-                    ${songTitle}</h3>
-                </h3>
+                <dl>  
+                    <dt>${date}</dt>
+                    <dd>${artist}:
+                    ${songTitle}</dd
+                </dl>
             `;
         });
         displayHtml += "</ul>";
