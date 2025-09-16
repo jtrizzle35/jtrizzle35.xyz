870b9eba7d37d63f343c2cb2fb04d3451dc2a1ae
jtrizzle35 <jtrizzle35@gmail.com>
2025-09-15 23:29:05 -0400

    Update script.js
    commented out the "my stuck songs" header

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ async function fetchAndDisplayStuckSongs() {
         return; // Exit if the element isn't found
     }
 
-    let displayHtml = "<h2>My Stuck Songs:</h2>";
+   // let displayHtml = "<h2>My Stuck Songs:</h2>";
     if (allStuckSongsData.length > 0) {
         displayHtml += "<ul>";
         allStuckSongsData.forEach(song => {
